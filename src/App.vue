f5462a9bfe843a151b77176bee7d986e6e4432e6
shuvo18103107 <18103107@iubat.edu>
2020-12-13 02:50:58 +0600

    📝 Delete operation and layout alignment has some bug

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
+    <router-view />
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Header from "./components/layout/Header";
 export default {
-  name: 'App',
+  name: "app",
   components: {
-    HelloWorld
-  }
-}
+    Header,
+  },
+};
 </script>
 
 <style>
@@ -23,6 +21,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
---
+++ src/views/About.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>This is the TodoList app v1.0.0. Made By Mohammad Ali Shuvo</p>
+  </div>
+</template>
